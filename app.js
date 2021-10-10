4b7cb9656295e640b5049f9499ea1e3cf4c3aa0e
chamindujs <chamindujs@gmail.com>
2021-10-10 22:43:36 +0530

    Add js

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-const nav = document.querySelector('nav')
-
-window.addEventListener('scroll', () => {
-    if (pageYOffset > 100) {
-        nav.style.backgroundColor = "var(--nav)"
-    } else {
-        nav.style.backgroundColor = "var(--nav)"
-    }
-})
+ 
